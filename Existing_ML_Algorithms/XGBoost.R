667d2b85691935b326af57bb2e791a8e70143e32
Ghanshyam Verma <ghanshyam.verma8@gmail.com>
2023-11-22 06:57:06 +0000

    Update XGBoost.R

--- Existing_ML_Algorithms/XGBoost.R
+++ Existing_ML_Algorithms/XGBoost.R
@@ -2,215 +2,231 @@
 # R version 3.6.2 or above
 
 # Required packages
-# caret 6.0.93
-# dplyr 1.1.0
-# e1071 1.7.11
-# xgboost 1.7.3.1
+required_packages <- c("caret", "dplyr", "e1071", "ggplot2", "class", 
+                       "randomForest", "pROC", "kernlab", "xgboost")
+
+# Check if required r libraries are already installed or not
+new_packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+
+# If required r packages or libraries are not already installed, Install them.
+if(length(new_packages)) install.packages(new_packages)
 
 # load packages
-library(caret) # for various machine learning functions
-library(dplyr) # for data manipulation
-library(e1071) # for various functions like confusion matrix
-library(xgboost) # for building XGboost model
+library(caret) 
+library(dplyr) 
+library(e1071) 
+library(ggplot2)
+library(class) 
+library(randomForest) 
+library(pROC) 
+library(kernlab) 
+library(xgboost)
+
+# Provide seed values for data partition and ml model building
+
+data_partition_seed <- 7
+ml_model_seed <- 1234
 
 
-############################### Input from user ##########################################
+# Define an input path
+input_path <- "/Datasets/Gene_Expression/"
 
-# read Gene expression data collected at two required time-points for all those subjects for which disease diagnosis need to be performed
-# required two time-points (t_1 = 0 hours i.e. healthy state and at time-point t_D i.e. diseased state or the time-point at which disease diagnosis has been requested)
-# if you want to use another another Gene Expression Dataset, just read another Gene Expression Dataset by giving path of that dataset in below statement.
-Gene_Exp_Data <- read.csv(file = "Gene_Expression_Dataset_4_GSE61754.csv", header = TRUE, sep=",", check.names = FALSE)
+# Define an output path
+output_path <- "/XGBoost/"
 
-# Display the data
-Gene_Exp_Data[c(1:7),c(1:7)] # show first 7 rows
-
-
-# go inside the train test split
-total_data <- Gene_Exp_Data
-
-Data_target <- total_data %>% filter(Time > 0) 
-
-## First dividing data into training and test set (single time point - target time point)
-set.seed(7)
-# Dividing data set into train (50%) and test (50%) using createDataPartition function of caret package
-index_test <- createDataPartition(y = Data_target$True_Class_Label,
-                                  p = 0.50, list = FALSE)
-test_data <- Data_target[index_test, ]
-train_data <- Data_target[-index_test, ]
-
-
-dim(train_data)
-dim(test_data)
-
-set.seed(7)
-# Dividing test data set further into holdout test set (25%) and validation set (25%) using createDataPartition function of caret package
-index_test <- createDataPartition(y = test_data$True_Class_Label,
-                                  p = 0.50, list = FALSE)
-hold_out_test <- test_data[index_test, ]
-valid_data <- test_data[-index_test, ]
-
-dim(hold_out_test)
-dim(valid_data)
-
-# full train data for final model building
-full_train_data <- rbind(train_data,valid_data)
-
-# train test all time points
-g_train_data <- total_data %>% filter(Super_Subject_ID %in% train_data$Super_Subject_ID)
-g_valid_data <- total_data %>% filter(Super_Subject_ID %in% valid_data$Super_Subject_ID)
-g_test_data <- total_data %>% filter(Super_Subject_ID %in% hold_out_test$Super_Subject_ID)
-g_full_train_data <- total_data %>% filter(Super_Subject_ID %in% full_train_data$Super_Subject_ID)
-
-
-
-# Display the dimensions of training and hold-out test set(rows columns)
-(dim(g_train_data))
-(dim(g_valid_data))
-(dim(g_test_data))
-(dim(g_full_train_data)) # for final model building
-
-# Display the data
-g_test_data[c(1:6),c(1:7)] # show first 6 rows
-
-
-# Train and test data
-g_train_data_all_genes <- g_train_data
-g_valid_data_all_genes <- g_valid_data
-g_test_data_all_genes <- g_test_data
-g_full_train_data_all_genes <- g_full_train_data
-
-# Converting Label vector into factor as per the requirement of train function of caret package
-Labels_g_train_data_all_genes <- as.factor(g_train_data$True_Class_Label)
-Labels_g_valid_data_all_genes <- as.factor(g_valid_data$True_Class_Label)
-Labels_g_test_data_all_genes <- as.factor(g_test_data$True_Class_Label)
-Labels_g_full_train_data_all_genes <- as.factor(g_full_train_data_all_genes$True_Class_Label)
-
-# Converting training and test data into matrix as per the requirement of train function of caret package
-g_train_data_all_genes <- as.matrix(g_train_data_all_genes[,-c(1:6)])
-g_valid_data_all_genes <- as.matrix(g_valid_data_all_genes[,-c(1:6)])
-g_test_data_all_genes <- as.matrix(g_test_data_all_genes[,-c(1:6)])
-g_full_train_data_all_genes <- as.matrix(g_full_train_data_all_genes[,-c(1:6)])
-
-
-Labels_g_train_data_all_genes <- recode(Labels_g_train_data_all_genes,'RVI'=1, 'Not RVI'=0)
-
-# training procedure
-
-set.seed(1234)
-train_full_Feature <- xgboost(data = g_train_data_all_genes, 
-                              label = Labels_g_train_data_all_genes, 
-                              max.depth = 6, 
-                              eta = 0.3, 
-                              nrounds = 100, 
-                              eval_metric = "error",
-                              objective = "binary:logistic",
-                              verbose = 1)
-
-
-
-
-
-# Print trained model
-print(train_full_Feature)
-
-# best performance on train (error)
-print(min(train_full_Feature$evaluation_log$train_error))
-
-# best performance on train (Accuracy)
-print(1-min(train_full_Feature$evaluation_log$train_error))
-
-##################################### Validation data prediction ####################################
-
-Labels_g_valid_data_all_genes <- recode(Labels_g_valid_data_all_genes,'RVI'=1, 'Not RVI'=0)
-
-# training procedure
-
-set.seed(1234)
-Valid_full_Feature <- xgboost(data = g_valid_data_all_genes, 
-                              label = Labels_g_valid_data_all_genes, 
-                              max.depth = 6, 
-                              eta = 0.3, 
-                              nrounds = 100, 
-                              eval_metric = "error",
-                              objective = "binary:logistic",
-                              verbose = 1)
-
-
-
-# Print valid model
-print(Valid_full_Feature)
-
-# best performance on valid (error)
-print(min(Valid_full_Feature$evaluation_log$train_error))
-
-# best performance on valid (Accuracy)
-print(1-min(Valid_full_Feature$evaluation_log$train_error))
-
-
-Valid_full_Feature$params
-
-
-###################### Final model building using full train data ####################################
-
-Labels_g_full_train_data_all_genes <- recode(Labels_g_full_train_data_all_genes,'RVI'=1, 'Not RVI'=0)
-
-# training procedure
-
-set.seed(1234)
-final_trained_model <- xgboost(data = g_full_train_data_all_genes, 
-                               label = Labels_g_full_train_data_all_genes, 
-                               max.depth = Valid_full_Feature$params$max_depth, 
-                               eta = Valid_full_Feature$params$eta, 
-                               nrounds = 100, 
-                               eval_metric = Valid_full_Feature$params$eval_metric,
-                               objective = Valid_full_Feature$params$objective,
-                               verbose = 1)
-
-
-
-# Print valid model
-print(final_trained_model)
-
-# best performance on valid (error)
-print(min(final_trained_model$evaluation_log$train_error))
-
-# best performance on valid (Accuracy)
-print(1-min(final_trained_model$evaluation_log$train_error))
-
-
-##################################### test data prediction Dataset 4 ####################################
-
-
-### Test set predicition
-# Converting Label vector into factor as per the requirement of train function of caret package
-Labels_g_test_data_time_t <- as.factor(hold_out_test$True_Class_Label)
-
-
-# Converting training and test data into matrix as per the requirement of train function of caret package
-g_test_data_time_t_all_genes <- as.matrix(hold_out_test[,-c(1:6)])
-
-# Predicting Test Data
-# Passing test data without labels (without fist column which contains labels)
-(testPrediction1 <- predict(final_trained_model, newdata = g_test_data_time_t_all_genes))
-
-for (i in 1:length(testPrediction1)) {
-  if(testPrediction1[i] < 0.5){
-    testPrediction1[i] <- 'Not RVI'
-  }else{
-    testPrediction1[i] <- 'RVI'
-  }
-  
+# Read Gene Expression Datasets
+read_gene_expression_data <- function(filename,input_path) {
+  path_filename <- paste0(input_path, filename)
+  return(read.csv(file = path_filename, header = TRUE, sep=",", check.names = FALSE))
 }
 
-# Show predicted labels
-print(testPrediction1)
-# Show Test Data
-print(Labels_g_test_data_time_t)
+# Split data into train, validation, and test
+split_data <- function(Gene_Exp_Data, data_partition_seed) {
+  total_data <- Gene_Exp_Data
+  Data_target <- total_data %>% filter(Time > 0)
+  
+  # Split into train and test
+  set.seed(data_partition_seed)
+  index_test <- createDataPartition(y = Data_target$True_Class_Label,
+                                    p = 0.50, list = FALSE)
+  test_data <- Data_target[index_test, ]
+  train_data <- Data_target[-index_test, ]
+  
+  dim(train_data)
+  dim(test_data)
+  
+  # Split test data into holdout test and validation
+  set.seed(data_partition_seed)
+  index_test <- createDataPartition(y = test_data$True_Class_Label,
+                                    p = 0.50, list = FALSE)
+  hold_out_test <- test_data[index_test, ]
+  valid_data <- test_data[-index_test, ]
+  dim(hold_out_test)
+  dim(valid_data)
+  
+  # Full train data for final model building
+  full_train_data <- rbind(train_data,valid_data)
+  
+  # train test all time points
+  g_train_data <- total_data %>% filter(Super_Subject_ID %in% train_data$Super_Subject_ID)
+  g_valid_data <- total_data %>% filter(Super_Subject_ID %in% valid_data$Super_Subject_ID)
+  g_test_data <- total_data %>% filter(Super_Subject_ID %in% hold_out_test$Super_Subject_ID)
+  g_full_train_data <- total_data %>% filter(Super_Subject_ID %in% full_train_data$Super_Subject_ID)
+  
+  # Display the data
+  g_test_data[c(1:6),c(1:7)] # show first 6 rows
+  
+  return(list(train_data = g_train_data, full_train_data = g_full_train_data, hold_out_test = hold_out_test, valid_data = g_valid_data))
+}
+
+# Train XGBoost model 
+train_XGBoost_model <- function(train_data, Labels_train_data, ml_model_seed) {
+  set.seed(ml_model_seed)
+  Labels_train_data <- recode(Labels_train_data,'RVI'=1, 'Not RVI'=0)
+  
+  # training procedure
+  trained_model <- xgboost(data = train_data, 
+                           label = Labels_train_data, 
+                           max.depth = 6, 
+                           eta = 0.3, 
+                           nrounds = 100, 
+                           eval_metric = "error",
+                           objective = "binary:logistic",
+                           verbose = 1)
+  
+  # Print trained model
+  return(trained_model)
+}
+
+# Final Training function for XGBoost using best parameters
+final_XGBoost_training_function <- function(train_data, Labels_train_data, ml_model_seed, final_max_depth, final_eta) {
+  set.seed(ml_model_seed)
+  Labels_train_data <- recode(Labels_train_data,'RVI'=1, 'Not RVI'=0)
+  
+  # training procedure
+  trained_model <- xgboost(data = train_data, 
+                           label = Labels_train_data, 
+                           max.depth = final_max_depth, 
+                           eta = final_eta, 
+                           nrounds = 100, 
+                           eval_metric = "error",
+                           objective = "binary:logistic",
+                           verbose = 1)
+  
+  # Print trained model
+  return(trained_model)
+}
+
+# Write confusion matrix results to TXT
+write_confusion_to_txt <- function(predictions, actual_labels, model_name, dataset_name, output_path) {
+  conf_matrix_result <- confusionMatrix(predictions, actual_labels)
+  
+  
+  # Extract the matrix and statistics
+  conf_matrix_table <- conf_matrix_result$table
+  overall_stats <- conf_matrix_result$overall
+  class_stats <- conf_matrix_result$byClass
+  
+  dataset_name <- substr(dataset_name, 1, nchar(dataset_name) - 4)
+  
+  # Create the filename
+  filename <- paste0(output_path, model_name, "_", dataset_name, "_confusion_matrix.txt")
+  
+  # Print confusion matrix
+  print(paste0("Print confusion matrix for hold-out test set for ", model_name, " ", "for ", dataset_name, ":"))
+  print(conf_matrix_result)
+  
+  # Open a connection for writing
+  con <- file(filename, "w")
+  
+  # Write the confusion matrix and stats to the text file
+  write("Confusion Matrix:", con)
+  write.table(conf_matrix_table, con, sep = "\t")
+  
+  write("\nOverall Statistics:", con)
+  write.table(as.data.frame(overall_stats), con, sep = "\t", row.names = TRUE)
+  
+  write("\nClass Statistics:", con)
+  write.table(as.data.frame(t(class_stats)), con, sep = "\t", row.names = TRUE)
+  
+  # Close the connection
+  close(con)
+}
 
 
-### Performance measure
 
-# Display confusion matrix Dataset 4
-print(confusionMatrix(as.factor(testPrediction1), Labels_g_test_data_time_t))
+# Main function
+main_function <- function() {
+  # Read datasets
+  dataset_names <- c("Gene_Expression_Dataset_4_GSE61754.csv")
+  for(dataset_name in dataset_names) {
+    Gene_Exp_Data <- read_gene_expression_data(dataset_name, input_path)
+    
+    # Split data
+    splits <- split_data(Gene_Exp_Data, data_partition_seed)
+    
+    # Code for XGBoost model building, validation, and evaluation
+    # Model building using training data
+    trained_XGBoost_model <- train_XGBoost_model(as.matrix(splits$train_data[,-c(1:6)]),
+                                                 as.factor(splits$train_data$True_Class_Label), ml_model_seed)
+    
+    # Print XGBoost training results
+    print("XGBoost training results:")
+    print(trained_XGBoost_model)
+    
+    # best performance on train data (error)
+    print(min(trained_XGBoost_model$evaluation_log$train_error))
+    
+    # best performance on train data (Accuracy)
+    print(1-min(trained_XGBoost_model$evaluation_log$train_error))
+    
+    # Performing validation and hyper parameter selection using validation data
+    validation_XGBoost_model <- train_XGBoost_model(as.matrix(splits$valid_data[,-c(1:6)]),
+                                                    as.factor(splits$valid_data$True_Class_Label), ml_model_seed)
+    
+    # Print XGBoost validation results
+    print("XGBoost validation results:")
+    print(validation_XGBoost_model)
+    
+    # Selecting final model parameters
+    final_max_depth <- validation_XGBoost_model$params$max_depth
+    final_eta <- validation_XGBoost_model$params$eta
+    
+    # best performance on valid (error)
+    print(min(validation_XGBoost_model$evaluation_log$train_error))
+    
+    # best performance on valid (Accuracy)
+    print(1-min(validation_XGBoost_model$evaluation_log$train_error))
+    
+    
+    # Print final parameter values
+    print("Final value of parameter max depth of XGBoost:")
+    print(final_max_depth)
+    print("Final value of parameter eta of XGBoost:")
+    print(final_eta)
 
+    
+    # Performing validation and hyper parameter selection using validation data
+    final_XGBoost_trained_model <- final_XGBoost_training_function(as.matrix(splits$full_train_data[,-c(1:6)]),
+                                                                   as.factor(splits$full_train_data$True_Class_Label),
+                                                                   ml_model_seed, final_max_depth, final_eta)
+    
+    # Test RF model
+    XGBoost_predictions <- predict(final_XGBoost_trained_model, newdata = as.matrix(splits$hold_out_test[,-c(1:6)]))
+    
+    for (i in 1:length(XGBoost_predictions)) {
+      if(XGBoost_predictions[i] < 0.5){
+        XGBoost_predictions[i] <- 'Not RVI'
+      }else{
+        XGBoost_predictions[i] <- 'RVI'
+      }
+      
+    }
+    
+    # Write results to a text file
+    write_confusion_to_txt(as.factor(XGBoost_predictions), as.factor(splits$hold_out_test$True_Class_Label), "XGBoost", dataset_name, output_path)
+    
+  }
+}
 
+main_function()
