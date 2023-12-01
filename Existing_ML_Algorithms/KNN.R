6acdf38d2a4a6069ae8a78b44261958485c1966a
Ghanshyam Verma <ghanshyam.verma8@gmail.com>
2023-12-01 08:02:33 +0000

    Update KNN.R

--- Existing_ML_Algorithms/KNN.R
+++ Existing_ML_Algorithms/KNN.R
@@ -32,7 +32,7 @@ ml_model_seed <- 1234
 input_path <- "/Datasets/Gene_Expression/"
 
 # Define an output path
-output_path <- "/KNN/"
+output_path <- "/All_ML_Models_Results/"
 
 # Read Gene Expression Datasets
 read_gene_expression_data <- function(filename,input_path) {
@@ -64,7 +64,7 @@ split_data <- function(Gene_Exp_Data, data_partition_seed) {
   dim(hold_out_test)
   dim(valid_data)
   
-  # full train data for final model building
+  # Full train data for final model building
   full_train_data <- rbind(train_data,valid_data)
   
   # train test all time points
@@ -108,38 +108,57 @@ final_knn_training_function <- function(train_data, Labels_train_data, ml_model_
 }
 
 
-
 # Write confusion matrix results to TXT
 write_confusion_to_txt <- function(predictions, actual_labels, model_name, dataset_name, output_path) {
-  conf_matrix_result <- confusionMatrix(predictions, actual_labels)
-  
-  
-  # Extract the matrix and statistics
-  conf_matrix_table <- conf_matrix_result$table
-  overall_stats <- conf_matrix_result$overall
-  class_stats <- conf_matrix_result$byClass
+  # Ensure that both predictions and actual_labels are factors
+  predictions <- factor(predictions)
+  actual_labels <- factor(actual_labels)
   
   dataset_name <- substr(dataset_name, 1, nchar(dataset_name) - 4)
   
   # Create the filename
   filename <- paste0(output_path, model_name, "_", dataset_name, "_confusion_matrix.txt")
   
-  # Print confusion matrix
-  print(paste0("Print confusion matrix for hold-out test set for ", model_name, " ", "for ", dataset_name, ":"))
-  print(conf_matrix_result)
-  
   # Open a connection for writing
   con <- file(filename, "w")
   
-  # Write the confusion matrix and stats to the text file
-  write("Confusion Matrix:", con)
-  write.table(conf_matrix_table, con, sep = "\t")
-  
-  write("\nOverall Statistics:", con)
-  write.table(as.data.frame(overall_stats), con, sep = "\t", row.names = TRUE)
-  
-  write("\nClass Statistics:", con)
-  write.table(as.data.frame(t(class_stats)), con, sep = "\t", row.names = TRUE)
+  if(length(levels(predictions)) == 1 && length(levels(actual_labels)) == 1) {
+    # Calculate accuracy for a single class
+    accuracy <- sum(predictions == actual_labels) / length(actual_labels)
+    write("Only one class present in both predictions and actual labels. Calculated accuracy for single class.", con)
+    write("Accuracy:", con)
+    write(accuracy, con)
+    
+    print("Only one class present in both predictions and actual labels.")
+    print(paste0("Calculated accuracy for single class: ", accuracy))
+    print(accuracy)
+  } else {
+    # Ensure both have the same levels
+    levels_union <- union(levels(predictions), levels(actual_labels))
+    predictions <- factor(predictions, levels = levels_union)
+    actual_labels <- factor(actual_labels, levels = levels_union)
+    
+    # Compute the confusion matrix
+    conf_matrix_result <- confusionMatrix(predictions, actual_labels)
+    
+    # Extract the matrix and statistics
+    conf_matrix_table <- conf_matrix_result$table
+    overall_stats <- conf_matrix_result$overall
+    class_stats <- conf_matrix_result$byClass
+    
+    # Print and write confusion matrix
+    print(paste0("Print confusion matrix for hold-out test set for ", model_name, " ", "for ", dataset_name, ":"))
+    print(conf_matrix_result)
+    
+    write("Confusion Matrix:", con)
+    write.table(conf_matrix_table, con, sep = "\t")
+    
+    write("\nOverall Statistics:", con)
+    write.table(as.data.frame(overall_stats), con, sep = "\t", row.names = TRUE)
+    
+    write("\nClass Statistics:", con)
+    write.table(as.data.frame(t(class_stats)), con, sep = "\t", row.names = TRUE)
+  }
   
   # Close the connection
   close(con)
@@ -150,7 +169,7 @@ write_confusion_to_txt <- function(predictions, actual_labels, model_name, datas
 # Main function
 main_function <- function() {
   # Read datasets
-  dataset_names <- c("Gene_Expression_Dataset_4_GSE61754.csv", "Gene_Expression_Dataset_3_GSE90732.csv", "Gene_Expression_Dataset_2_GSE68310.csv", "Gene_Expression_Dataset_1_GSE73072.csv")
+  dataset_names <- c("Gene_Expression_Dataset_1_GSE73072.csv", "Gene_Expression_Dataset_2_GSE68310.csv", "Gene_Expression_Dataset_3_GSE90732.csv", "Gene_Expression_Dataset_4_GSE61754.csv")
   for(dataset_name in dataset_names) {
     Gene_Exp_Data <- read_gene_expression_data(dataset_name, input_path)
     
@@ -160,36 +179,39 @@ main_function <- function() {
     # Model building using training data
     trained_knn_model <- train_knn_model(as.matrix(splits$train_data[,-c(1:6)]),
                                          as.factor(splits$train_data$True_Class_Label), ml_model_seed)
-    
+
     # Print KNN training results
     print("KNN training results:")
     print(trained_knn_model)
-    
-    # Performing validation and hyperparameter selection using validation data
+
+    # Performing validation and hyper parameter selection using validation data
     validation_knn_model <- train_knn_model(as.matrix(splits$valid_data[,-c(1:6)]),
                                             as.factor(splits$valid_data$True_Class_Label), ml_model_seed)
-    
-    
+
+
     # Print KNN validation results
     print("KNN validation results:")
     print(validation_knn_model)
-    
+
     # Selecting final model parameters
     final_k <- validation_knn_model$finalModel$tuneValue[1]
-    
-    # Print final parameter values 
-    print
-    
-    # Building the final model using the best-selected hyperparameters
+
+    # Print final parameter values
+    print("Final value of k:")
+    print(final_k)
+
+    # Final model building using KNN
     final_knn_trained_model <- final_knn_training_function(as.matrix(splits$full_train_data[,-c(1:6)]),
-                                                           as.factor(splits$full_train_data$True_Class_Label), 
+                                                           as.factor(splits$full_train_data$True_Class_Label),
                                                            ml_model_seed, final_k)
-    
+
+
     # Test KNN
     knn_predictions <- predict(final_knn_trained_model, newdata = as.matrix(splits$hold_out_test[,-c(1:6)]))
-    write_confusion_to_txt(knn_predictions, as.factor(splits$hold_out_test$True_Class_Label), "KNN", dataset_name, output_path)
-    
 
+    # Write results to a text file
+    write_confusion_to_txt(knn_predictions, as.factor(splits$hold_out_test$True_Class_Label), "KNN", dataset_name, output_path)
+  
   }
 }
 
